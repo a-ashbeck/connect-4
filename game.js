8daf9ff1e04356a4c1d6b0d36d611e49768fcb49
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-10-24 19:52:24 -0500

    Add multidimensional array initialization, setter and getter

---
+++ game.js
@@ -0,0 +1,29 @@
+/*
+CONNECT 4
+ */
+
+var gamePieceArray = [];
+
+// gamePieceArray will have three possible states 'empty', 'player1', 'player2'
+// initialize gamePieceArray so that all indices are set to 'empty'
+function initializeBoard() {
+  var iMax = 6;
+  var jMax = 5;
+  for (i = 0; i < iMax; i++) {
+    gamePieceArray[i] = [];
+
+    for (j = 0; j < jMax; j++) {
+       gamePieceArray[i][j] = 'empty';
+    }
+  }
+}
+
+// set state of cell
+function setPiece(player, column, row) {
+  gamePieceArray[column][row] = player;
+}
+
+// get state of cell
+function getPiece(column, row) {
+  return gamePieceArray[column][row];
+}
