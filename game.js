c82853baa79ee01b66a0129ebf179b220e301a2c
Alan <a-ashbeck@northwestern.edu>
2016-10-24 21:21:46 -0500

    Update logic

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ var gamePieceArray = [];
 // initialize gamePieceArray so that all indices are set to 'empty'
 function initializeBoard() {
   var iMax = 6;
-  var jMax = 5;
+  var jMax = 7;
   for (i = 0; i < iMax; i++) {
     gamePieceArray[i] = [];
 
@@ -19,27 +19,46 @@ function initializeBoard() {
 }
 
 // set state of cell
-function setPiece(player, column, row) {
-  gamePieceArray[column][row] = player;
+function setPiece(player, row, column) {
+  gamePieceArray[row][column] = player;
 }
 
 // get state of cell
-function getState(column, row) {
-  return gamePieceArray[column][row];
+function getState(row, column) {
+  return gamePieceArray[row][column];
+}
+
+function isBelowEmpty(row, column) {
+  console.log(gamePieceArray);
+  console.log(row);
+  if ((gamePieceArray[row + 1][column] !== 'empty') || (row + 1 === 5)) {
+    return row;
+  } else {
+    return isBelowEmpty(row + 1, column);
+
+  };
 }
 
 
 $(document).ready(function() {
-    var user = 1;
+    var user = 'player1';
     initializeBoard();
 
     $('.cell').on('click', function() {
         var cellId = $(this).attr('id');
-        var column = parseInt(cellId[0]) - 1;
-        var row = parseInt(cellId[2]) - 1;
-        var state = getState(column, row);
-        if (state === 'empty') {
-            $(this).css({'background-color': 'red'});
+        var row = parseInt(cellId[0]) - 1;
+        var column = parseInt(cellId[2]) - 1;
+        var state = getState(row, column);
+        if ((state === 'empty') && (user === 'player1')) {
+            $('#' + isBelowEmpty(row, column) + 'x' + column).css({'background-color': 'red'});
+            console.log('HERE!');
+            setPiece(user, isBelowEmpty(row, column), column);
+            user = 'player2';
+        } else if ((state === 'empty') && (user === 'player2')) {
+            $(this).css({'background-color': 'black'});
+            setPiece(user, isBelowEmpty(row, column), column);
+            user = 'player1';
         }
+
     });
 });
